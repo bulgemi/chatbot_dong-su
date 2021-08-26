9ed8465f3dd2413f85c035bba5e2791c37dd8f9a
donghun <bulgemi77@gmail.com>
2021-08-26 18:30:57 +0900

    메서드 추가.

--- MlPipeLine/utils/Preprocess.py
+++ MlPipeLine/utils/Preprocess.py
@@ -1,15 +1,23 @@
 # _*_ coding: utf-8 _*_
 __author__ = 'kim dong-hun'
 from konlpy.tag import Komoran
+import pickle
 
 
 class Preprocess(object):
-    def __init__(self, userdic=None):
+    def __init__(self, word2index_dic='', userdic=None):
         """
         형태소 분석기 초기화
         제외할 품사 정의
+        :param word2index:
         :param userdic:
         """
+        # 단어 인덱스 사전 불러오기
+        self.word_index = None
+        if word2index_dic != '':
+            f = open(word2index_dic, 'rb')
+            self.word_index = pickle.load(f)
+            f.close()
         self.komoran = Komoran(userdic=userdic)
         self.exclusion_tags = [
             'JKS', 'JKC', 'JKG', 'JKO', 'JKB', 'JKV', 'JKQ', 'JX', 'JC',
@@ -39,3 +47,20 @@ class Preprocess(object):
             if f(p[1]) is False:
                 word_list.append(p if without_tag is False else p[0])
         return word_list
+
+    def get_wordidx_sequence(self, keywords):
+        """
+        키워드로 단어 인덱스 시퀀스 변환
+        :param keywords:
+        :return:
+        """
+        if self.word_index is None:
+            return []
+        w2i = []
+        for word in keywords:
+            try:
+                w2i.append(self.word_index[word])
+            except KeyError:
+                # 해당 단어가 없을 경우 OOV 처리
+                w2i.append(self.word_index['OOV'])
+        return w2i
