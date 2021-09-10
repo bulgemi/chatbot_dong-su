dce3441124e923df2936c145fb029758c87426a3
donghun <bulgemi77@gmail.com>
2021-09-10 09:53:25 +0900

    DB 모델 추가.

---
+++ MlPipeLine/config/tables.py
@@ -0,0 +1,22 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'kim dong-hun'
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Text
+
+Base = declarative_base()
+
+
+class ChatbotTrainData(Base):
+    __tablename__ = 'chatbot_train_data'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    intent = Column(String(45))
+    ner = Column(String(1024))
+    query = Column(Text)
+    answer = Column(Text)
+    answer_image = Column(String(2048))
+
+    def __repr__(self):
+        return "<ChatbotTrainData(id='%s', intent='%s', ner='%s')>" % (self.id, self.intent, self.ner)
