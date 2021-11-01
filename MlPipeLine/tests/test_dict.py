abc7c3bf6642e397ec1b5672255bb2580a25ad0d
donghun <bulgemi77@gmail.com>
2021-11-01 16:56:09 +0900

    테스트 케이스 추가.

--- MlPipeLine/tests/test_dict.py
+++ MlPipeLine/tests/test_dict.py
@@ -13,17 +13,22 @@ def test_dict():
     word_index = pickle.load(f)
     f.close()
 
-    sent = "내일 오전 10시에 탕수육 주문하고 싶어 ㅋㅋ"
+    sent = ["네임스페이스 목록 보여줘",
+            "namespace 목록 보여줘",
+            "namespace 리스트 알려줘",
+            "Namespace 목록 보여줘"]
 
     # 전처리 객체 생성
     p = Preprocess(userdic='../train_tools/dict/user_dic.tsv')
     # 형태소 분석기 실행
-    pos = p.pos(sent)
-    # 품사 태그 없이 키워드 출력
-    keywords = p.get_keywords(pos, without_tag=True)
-    for word in keywords:
-        try:
-            print(word, word_index[word])
-        except KeyError:
-            # 해당 언어가 사전에 없는 경우 OOV 처리
-            print(word, word_index['OOV'])
+    for s in sent:
+        pos = p.pos(s)
+        print(pos)
+        # 품사 태그 없이 키워드 출력
+        keywords = p.get_keywords(pos, without_tag=True)
+        for word in keywords:
+            try:
+                print(word, word_index[word])
+            except KeyError:
+                # 해당 언어가 사전에 없는 경우 OOV 처리
+                print(word, word_index['OOV'])
